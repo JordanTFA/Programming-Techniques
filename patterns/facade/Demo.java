cc05bade119b2548ace5e5a4f29cb71c8d1a0909
JordanTFA <Jordantfa@hotmail.co.uk>
2018-07-13 23:28:33 +0100

    Changed text to another class

--- patterns/facade/Demo.java
+++ patterns/facade/Demo.java
@@ -1,5 +1,19 @@
 package facade;
 
+/* https://www.tutorialspoint.com/design_pattern/facade_pattern.htm
+ * 
+ * Facade pattern hides the complexities of the system 
+ * and provides an interface to the client using which 
+ * the client can access the system. This type of design 
+ * pattern comes under structural pattern as this pattern 
+ * adds an interface to existing system to hide its 
+ * complexities.
+ * 
+ * This pattern involves a single class which provides 
+ * simplified methods required by client and delegates 
+ * calls to methods of existing system classes.
+ */
+
 public class Demo {
 
 	public static void main(String[] args) {
