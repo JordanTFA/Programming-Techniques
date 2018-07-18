c6da1e4ac1cc0cdef822481390d009434fe18f30
JordanTFA <Jordantfa@hotmail.co.uk>
2018-07-18 10:49:42 +0100

    Added Command pattern

---
+++ patterns/command/Stock.java
@@ -0,0 +1,27 @@
+package command;
+
+/* https://www.tutorialspoint.com/design_pattern/command_pattern.htm
+ * 
+ * Command pattern is a data driven design pattern and falls 
+ * under behavioral pattern category. A request is wrapped 
+ * under an object as command and passed to invoker object. 
+ * Invoker object looks for the appropriate object which can 
+ * handle this command and passes the command to the 
+ * corresponding object which executes the command.
+ */
+
+public class Stock {
+	
+	private String name = "ABC";
+	private int quantity = 10;
+	
+	public void buy(){
+		
+		System.out.println("Stock - Name: " + name + ", Quantity: " + quantity + " bought.");
+	}
+	
+	public void sell(){
+		System.out.println("Stock - Name: " + name + ", Quantity: " + quantity + " sold.");
+	}
+
+}
