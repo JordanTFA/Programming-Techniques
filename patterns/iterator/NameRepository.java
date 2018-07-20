a36d8f4442db39ebdb7c27fe2cfd7db137e5cf06
JordanTFA <Jordantfa@hotmail.co.uk>
2018-07-20 23:12:19 +0100

    Added Iterator pattern

---
+++ patterns/iterator/NameRepository.java
@@ -0,0 +1,39 @@
+package iterator;
+
+public class NameRepository implements Container {
+	
+	public String names[] = {"Robert", "John", "Julia", "Laura"};
+
+	@Override
+	public Iterator getIterator() {
+		return new NameIterator();
+	}
+
+	private class NameIterator implements Iterator{
+		
+		int index;
+		
+		@Override
+		public boolean hasNext(){
+			
+			if(index < names.length){
+				return true;
+			}
+			
+			return false;
+		}
+		
+		@Override
+		public Object next(){
+			
+			if (this.hasNext()){
+				return names[index++];
+			}
+			
+			return null;
+		}
+	}
+
+
+
+}
