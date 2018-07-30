e71cf1117c247d0f48b1fb6289a3133b8daaccb5
JordanTFA <Jordantfa@hotmail.co.uk>
2018-07-30 23:23:59 +0100

    All patterns complete

---
+++ patterns/mvc/Demo.java
@@ -0,0 +1,45 @@
+package mvc;
+
+/* https://www.tutorialspoint.com/design_pattern/mvc_pattern.htm
+ * 
+ * MVC Pattern stands for Model-View-Controller Pattern. This pattern is used to separate application's concerns.
+ * 
+ * Model - Model represents an object or JAVA POJO carrying data. It can also have logic to update controller if its data changes.
+ *
+ * View - View represents the visualization of the data that model contains.
+ * 
+ * Controller - Controller acts on both model and view. It controls the data flow into model object and updates the view whenever data 
+ * changes. It keeps view and model separate.
+ */
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		// Fetch student record based on his roll no from the database
+		Student model = retrieveStudentFromDatabase();
+		
+		// Create a view to write student detail to console
+		StudentView view = new StudentView();
+		
+		StudentController controller = new StudentController(model, view);
+		
+		controller.updateView();
+		
+		// Update model data
+		controller.setStudentName("John");
+		
+		
+		controller.updateView();
+
+	}
+
+	private static Student retrieveStudentFromDatabase() {
+		
+		Student student = new Student();
+		student.setName("Robert");
+		student.setRollNo("10");
+		return student;
+	}
+
+}
