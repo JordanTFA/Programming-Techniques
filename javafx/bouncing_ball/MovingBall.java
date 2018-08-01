a3f94df98e953e29389ee1756eb05ed5f7c29d67
JordanTFA <Jordantfa@hotmail.co.uk>
2018-08-01 23:40:25 +0100

    Added Moving Ball

---
+++ javafx/bouncing_ball/MovingBall.java
@@ -0,0 +1,47 @@
+package bouncing_ball;
+
+/*
+ * https://www.mkyong.com/javafx/javafx-animated-ball-example/
+ */
+
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.geometry.Bounds;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class MovingBall extends Application{
+
+	public static void main(String[] args) {
+
+		launch();
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+		Pane canvas = new Pane();
+		Scene scene = new Scene(canvas, 300, 300);
+		Circle ball = new Circle(10, Color.RED);
+		ball.relocate(0, 10);
+		
+		canvas.getChildren().add(ball);
+		
+		stage.setTitle("Moving Ball");;
+		stage.setScene(scene);
+		stage.show();
+		
+		Bounds bounds = canvas.getBoundsInLocal();
+		Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(3), 
+                new KeyValue(ball.layoutXProperty(), bounds.getMaxX()-ball.getRadius())));
+		timeline.setCycleCount(2);
+		timeline.play();
+	}
+
+}
